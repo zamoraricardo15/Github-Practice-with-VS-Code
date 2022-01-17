6f72fdaef7f2b63b24675d3f8c0958059ea228e5
Ricardo Zamora Mennigke <87404776+zamoraricardo15@users.noreply.github.com>
2022-01-16 20:05:31 -0600

    Add files via upload

---
+++ Assignment-Functions-Intermediate-I/AssignmentFunctionsIntermediateI.py
@@ -0,0 +1,94 @@
+#Assignment: Functions Intermediate I
+
+# Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+x[1][0] = 15
+print(x)
+
+students[0]['last_name'] = "Bryant"
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print( sports_directory['soccer'])
+
+z[0]['y'] = 30
+print(z)
+
+
+# Iterate Through a List of Dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key,val in list[i].items():
+            output += f" {key} - {val},"
+        print(output)
+
+iterate_dictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+# # Get Values From a List of Dictionaries
+# Michael
+# John
+# Mark
+# KB
+
+# Jordan
+# Rosales
+# Guillen
+# Tonel
+
+#Iterate Through a Dictionary with List Values
+dojo = {
+'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(dict):
+    for key,val in dict.items():
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+print_info(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
---
+++ Assignment-Functions-Intermediate-I/README.md
@@ -0,0 +1,1 @@
+# Assignment-Functions-Intermediate-I
